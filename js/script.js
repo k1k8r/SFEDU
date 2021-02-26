5cf855ba6eb9e5e84a26d5e272fb03f77beb634f
k1k8r <andreyzadonsky02@mail.ru>
2021-02-26 14:18:05 +0300

    master: .....

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 	$('.header-burger').click(function (event) {
 		$('.header-burger,.header-menu').toggleClass('active');
 		$('body').toggleClass('lock');
-	})
+	});
 });
 
 $(document).ready(function () {
